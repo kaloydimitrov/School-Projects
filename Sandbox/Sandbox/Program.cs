36a023a17da3ff35002327a22b20281acc7f0fff
kaloydimitrov <100486624+kaloydimitrov@users.noreply.github.com>
2023-10-06 00:00:23 +0300

    all done

--- Sandbox/Sandbox/Program.cs
+++ Sandbox/Sandbox/Program.cs
@@ -1,4 +1,4 @@
-﻿double a, b, c, P, p;
+﻿double a, b, c, P, p, S;
 
 a = double.Parse(Console.ReadLine());
 b = double.Parse(Console.ReadLine());
@@ -6,3 +6,6 @@ c = double.Parse(Console.ReadLine());
 
 P = a + b + c;
 p = P / 2;
+S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+Console.WriteLine(S);
